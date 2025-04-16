eb68068f9804dc093f0aaf9f644808c9b90cba4b
JarolVidal9 <jarol.vidal@utp.edu.co>
2025-04-16 01:10:23 -0500

    refactor(layout): update layout structure by replacing router-outlet with self-closing tag and adding margin div

---
+++ src/app/features/goals/create-edit/create-edit.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-edit',
+  imports: [],
+  templateUrl: './create-edit.component.html',
+  styleUrl: './create-edit.component.css'
+})
+export class CreateEditComponent {
+
+}
